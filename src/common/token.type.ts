d3667a856b781ff08124feb5a711468105b884c9
eyoon <euiseonyoon@gmail.com>
2025-11-29 11:50:59 +0900

    refactor: change Token to Class

--- src/common/token.type.ts
+++ src/common/token.type.ts
@@ -1,12 +1,22 @@
 "address"
+import Decimal from "decimal.js";
 import { ChainInfo } from "./chain-info.type"
 import { EvmAddress } from "./evm-address.class"
 
-export type Token = {
-    chain: ChainInfo,
-    address: EvmAddress,
-    symbol: string,
-    decimals: number,
-    name: string,
-    logoUri?: string
+export class Token {
+    constructor(
+        public readonly chain: ChainInfo,
+        public readonly address: EvmAddress,
+        public readonly symbol: string,
+        public readonly decimals: number,
+        public readonly name: string,
+        public readonly logoUri: string | undefined
+    ) {}
+
+    convertToBigIntAmount(amount: string): bigint {
+        const amountInDecimal: Decimal = new Decimal(amount);
+        const weiDecimal: Decimal = amountInDecimal.times(new Decimal(10).toPower(this.decimals));
+        const weiString: string = weiDecimal.toFixed(0, Decimal.ROUND_DOWN);
+        return BigInt(weiString);
+    }
 }
