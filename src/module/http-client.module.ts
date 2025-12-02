ecb2a029d54504799268138af3ead6400b094b39
eyoon <euiseonyoon@gmail.com>
2025-12-02 13:37:25 +0900

    feat: HttpClientModule, TxModule 수정

--- src/module/http-client.module.ts
+++ src/module/http-client.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AxiosHttpClientAdapter } from '../adapter/secondary/http-client/axios-http-client.adapter';
 
+export const HTTP_CLIENT = "HttpClient"
+
 @Module({
     providers: [
         {
-            provide: 'HttpClient',
+            provide: HTTP_CLIENT,
             useClass: AxiosHttpClientAdapter,
         },
     ],
-    exports: ['HttpClient'],
+    exports: [
+        HTTP_CLIENT,
+    ],
 })
 export class HttpClientModule {}
--- src/module/tx.module.ts
+++ src/module/tx.module.ts
@@ -3,9 +3,14 @@ import { TxController } from 'src/adapter/primary/transaction/tx.controller';
 import { TxServiceImpl } from 'src/application/transaction/tx.service';
 import { PublicClientModule } from './public-client.module';
 
+export const TX_SERVICE = 'TxService';
+
 @Module({
     imports: [PublicClientModule],
     controllers: [TxController],
-    providers: [{ provide: 'TxService', useClass: TxServiceImpl }],
+    providers: [{ provide: TX_SERVICE, useClass: TxServiceImpl }],
+    exports: [
+        TX_SERVICE
+    ]
 })
 export class TxModule {}
