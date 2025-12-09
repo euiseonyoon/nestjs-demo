5b86fa8a45b8e01160d30af8113ffb538da869c8
eyoon <euiseonyoon@gmail.com>
2025-12-09 21:25:16 +0900

    feat: TxService.getTxReceipt에 예외 추가

--- src/application/transaction/tx.service.ts
+++ src/application/transaction/tx.service.ts
@@ -1,5 +1,5 @@
 import { BadRequestException, Inject, Injectable } from '@nestjs/common';
-import { Hash, TransactionReceipt } from 'viem';
+import { Hash, TransactionReceipt, TransactionReceiptNotFoundError } from 'viem';
 import * as chains from 'viem/chains';
 import { ITxService } from './provided_port/tx.provided-port';
 import { EvmTxHash } from 'src/domain/evm-tx-hash.class';
@@ -22,16 +22,23 @@ export class TxService implements ITxService {
         chainId: number,
     ): Promise<TransactionReceipt | null> {
         const chain = this.getChainById(chainId);
-
         if (chain === undefined) {
             throw new BadRequestException('지원하지 않는 체인입니다.');
         }
 
         const client = this.rpcClientManager.getRpcClient(chain.id);
-        if (!client) return null
+        if (!client) return null;
 
-        const receipt = await client.getTransactionReceipt({ hash: txHash.toViemHash()});
-
-        return receipt;
+        try {
+            const receipt = await client.getTransactionReceipt({
+                hash: txHash.toViemHash()
+            });
+            return receipt;
+        } catch (error) {
+            if (error instanceof TransactionReceiptNotFoundError) {
+                return null;  // pending이거나 존재하지 않음
+            }
+            throw error;  // 다른 에러는 그대로 전파
+        }
     }
 }
