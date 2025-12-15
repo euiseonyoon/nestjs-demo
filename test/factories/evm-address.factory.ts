660e561db83c904ba3941a5b4273812c973915aa
eyoon <euiseonyoon@gmail.com>
2025-12-15 14:34:46 +0900

    test: generate random hex

--- test/factories/evm-address.factory.ts
+++ test/factories/evm-address.factory.ts
@@ -1,9 +1,10 @@
 import { EvmAddress } from "src/domain/evm-address.class";
+import { generateRandomHexString } from "./random.hex.generator";
 
 export function createTestEvmAddress(
     address?: string,
 ): EvmAddress {
     return new EvmAddress(
-        address ?? "0xabcde12345abcde12345abcde12345abcde12345"
+        address ?? generateRandomHexString(40)
     )
 }
