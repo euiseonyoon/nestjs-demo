432c8e128afb8dea99886c70cfe6e4fec59a5a04
eyoon <euiseonyoon@gmail.com>
2025-12-17 09:12:53 +0900

    feat: rename DefiType and make more types

--- src/domain/defi-type.enum.ts
+++ src/domain/defi-type.enum.ts
@@ -1,4 +1,34 @@
-export enum DefiType {
+export enum ProtocolType {
     SWAP = "SWAP",
     BRIDGE = "BRIDGE",
 }
+
+export type ProtocolInfo = {
+    name: string,
+    type: ProtocolType,
+    version: string,
+}
+
+const oneInchClassicSwapProtocolInfo = {
+    name: "OneInchClassicSwap",
+    type: ProtocolType.SWAP,
+    version: "V1",
+} as ProtocolInfo
+
+const sushiSwapSimpleProtocolInfo = {
+    name: "SushiSwapSimple",
+    type: ProtocolType.SWAP,
+    version: "V1",
+} as ProtocolInfo
+
+const stargateBridgeProtocolInfo = {
+    name: "StargateBridge",
+    type: ProtocolType.BRIDGE,
+    version: "V2",
+} as ProtocolInfo
+
+export const possibleProtocolInfo = {
+    oneInchClassicSwapProtocolInfo,
+    sushiSwapSimpleProtocolInfo,
+    stargateBridgeProtocolInfo
+} as const
