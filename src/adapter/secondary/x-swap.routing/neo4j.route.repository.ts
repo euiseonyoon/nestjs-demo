6381a521d8ba0453c35bca8844fbaa401a158bf4
eyoon <euiseonyoon@gmail.com>
2025-12-17 15:26:35 +0900

    feat: `Neo4JRouteRepository` created

---
+++ src/adapter/secondary/x-swap.routing/neo4j.route.repository.ts
@@ -0,0 +1,146 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { Token } from "src/domain/token.class";
+import { NEO4J_ADAPTER,  } from "src/infrastructure/infrastructure.token";
+import { type INeo4JAdapter } from "../neo4j/provided_port/neo4j.adapter.interface";
+import { ProtocolInfo } from "src/domain/defi-type.enum";
+import { Route } from "src/domain/x-swap.type";
+import { ManagedTransaction } from "neo4j-driver";
+import { X_SWAP_CHAIN_REPOSITORY, X_SWAP_PROTOCOL_REPOSITORY, X_SWAP_TOKEN_REPOSITORY } from "src/module/module.token";
+import { IXSwapStepRepository } from "src/application/x-swap.routing/required_port/x-swap.route.repository";
+import { type IXSwapTokenRepository } from "src/application/x-swap.routing/required_port/x-swap.token.repository";
+import { type IXSwapProtocolRepository } from "src/application/x-swap.routing/required_port/x-swap.protocol.repository";
+import Cypher from "@neo4j/cypher-builder";
+import { type IXSwapChainRepository } from "src/application/x-swap.routing/required_port/x-swap.chain.repository";
+
+@Injectable()
+export class Neo4JRouteRepository implements IXSwapStepRepository {
+    constructor(
+        @Inject(NEO4J_ADAPTER)
+        private readonly adapter: INeo4JAdapter,
+        @Inject(X_SWAP_TOKEN_REPOSITORY)
+        private readonly tokenRepository: IXSwapTokenRepository<ManagedTransaction, string>,
+        @Inject(X_SWAP_PROTOCOL_REPOSITORY)
+        private readonly protocolRepository: IXSwapProtocolRepository<ManagedTransaction, string>,
+        @Inject(X_SWAP_CHAIN_REPOSITORY)
+        private readonly chainRepository: IXSwapChainRepository<ManagedTransaction, number>
+    ) {}
+    
+    async saveRoute(route: Route): Promise<void> {
+        const session = this.adapter.getSession()
+
+        try {
+            await session.executeWrite(async (tx) => {
+                for (const step of route.steps) {
+                    await this.saveStep(
+                        step.srcToken,
+                        step.dstToken,
+                        step.protocolInfo,
+                        tx
+                    )
+                }
+            });
+        } catch (error) {
+            console.log("[ERROR]" + error)
+            throw error
+        } finally {
+            session.close()
+        }
+    }
+
+    private async saveRelationship(
+        tx: ManagedTransaction, 
+        protocolInfo: ProtocolInfo,
+        srcTokenId: string,
+        dstTokenId: string,
+        protocolId: string,
+    ) {
+        // 1. 새로운 Cypher.Node 객체 생성 (쿼리 빌더용)
+        const srcNode = new Cypher.Node();
+        const dstNode = new Cypher.Node();
+        const srcToDstRel = new Cypher.Relationship();
+        const dstToSrcRel = new Cypher.Relationship();
+        
+        // 체이닝으로 연결
+        const query = new Cypher.Match(
+            new Cypher.Pattern(srcNode, {
+                labels: ['Token'],
+                properties: { tokenId: new Cypher.Param(srcTokenId) }
+            })
+        )
+        .match(
+            new Cypher.Pattern(dstNode, {
+                labels: ['Token'],
+                properties: { tokenId: new Cypher.Param(dstTokenId) }
+            })
+        )
+        // srcNode -> dstNode  swap/bridge relation 생성
+        .merge(
+            new Cypher.Pattern(srcNode)
+                .related(srcToDstRel, {
+                    type: protocolInfo.type,
+                    properties: { protocolId: new Cypher.Param(protocolId) }
+                })
+                .to(dstNode)
+        )
+        .onCreateSet(
+            [srcToDstRel.property('protocol'), new Cypher.Param(protocolInfo.name)],
+            [srcToDstRel.property('version'), new Cypher.Param(protocolInfo.version)],
+        )
+        // dstNode -> srcNode  swap/bridge relation 생성 (Swap, Bridge는 일반적으로 양방향 가능하기 때문에.)
+        .merge(
+            new Cypher.Pattern(dstNode)
+                .related(dstToSrcRel, {
+                    type: protocolInfo.type,
+                    properties: { protocolId: new Cypher.Param(protocolId) }
+                })
+                .to(srcNode)
+        )
+        .onCreateSet(
+            [dstToSrcRel.property('protocol'), new Cypher.Param(protocolInfo.name)],
+            [dstToSrcRel.property('version'), new Cypher.Param(protocolInfo.version)],
+        );
+        /**
+         * 
+            MATCH (this0:Token { tokenId: $param0 })
+            MATCH (this1:Token { tokenId: $param1 })
+            MERGE (this0)-[this2:SWAP { protocolId: $param2 }]->(this1)
+            ON CREATE SET
+                this2.protocol = $param3,
+                this2.version = $param4
+            MERGE (this1)-[this3:SWAP { protocolId: $param5 }]->(this0)
+            ON CREATE SET
+                this3.protocol = $param6,
+                this3.version = $param7
+         */
+
+        const { cypher, params } = query.build()
+
+        await tx.run(cypher, params);
+    }
+
+    private async saveStep(
+        srcToken: Token, 
+        dstToken: Token, 
+        protocolInfo: ProtocolInfo,
+        tx: ManagedTransaction,
+    ): Promise<void> {
+        try {
+            if (srcToken.chain.id === dstToken.chain.id){
+                await this.chainRepository.saveIfNotExists(srcToken.chain, tx)
+            } else {
+                await this.chainRepository.saveIfNotExists(srcToken.chain, tx)
+                await this.chainRepository.saveIfNotExists(dstToken.chain, tx)
+            }
+
+            const srcTokenId = await this.tokenRepository.saveIfNotExists(srcToken, tx)
+            const dstTokenId = await this.tokenRepository.saveIfNotExists(dstToken, tx)
+
+            const protocolId = await this.protocolRepository.saveIfNotExists(protocolInfo, tx)
+
+            await this.saveRelationship(tx, protocolInfo, srcTokenId, dstTokenId, protocolId)
+        } catch (error) {
+            console.log("[ERROR]" + error)
+            throw error
+        }
+    }
+}
--- src/application/x-swap.routing/required_port/x-swap.route.repository.ts
+++ src/application/x-swap.routing/required_port/x-swap.route.repository.ts
@@ -1,7 +1,5 @@
-import { ProtocolInfo } from "src/domain/defi-type.enum";
-import { Token } from "src/domain/token.class";
-import { Route, RouteStep } from "src/domain/x-swap.type";
+import { Route } from "src/domain/x-swap.type";
 
-export interface IXSwapStepRepository<T, TReturn> {
+export interface IXSwapStepRepository {
     saveRoute (route: Route): Promise<void>
 }
