ef1c7ca2727688f12402c191fbef3d8112ddc292
eyoon <euiseonyoon@gmail.com>
2025-12-19 04:34:23 +0900

    feat: apply `OneInchInfoProviderTokenCacheInstance`

--- src/application/defi.info-provider/swap/1inch/1inch.info-provider.ts
+++ src/application/defi.info-provider/swap/1inch/1inch.info-provider.ts
@@ -2,15 +2,19 @@ import { Injectable, Inject, OnModuleInit } from '@nestjs/common';
 import { Token } from 'src/domain/token.class';
 import { Cron } from '@nestjs/schedule';
 import { EvmAddress } from 'src/domain/evm-address.class';
-import { HTTP_CLIENT, ONE_INCH_INFO_FETCHER } from 'src/module/module.token';
+import { CACHE_REGISTRY, HTTP_CLIENT, ONE_INCH_INFO_FETCHER, ONE_INCH_INFO_PROVIDER_TOKEN_CACHE_NAME, ONE_INCH_INFO_PROVIDER_TOKEN_CACHE_KEY_GENERATOR } from 'src/module/module.token';
 import { AbstractDefiProtocolInfoProvider } from '../../provided_port/defi-info-provider.interface';
 import { ChainInfo } from 'src/domain/chain-info.type';
 import { type IHttpClient } from '../../../common/required_port/http-client.interface';
 import { type IOneInchInfoFetcher } from 'src/application/defi.info-fetcher/swap/1inch/provided_port/1inch-swap.info-fetcher.interface';
+import { KeyInput } from 'src/application/cache/key.generator/1inch.info-provider.token-cache.key.generator';
+import { type ICacheKeyGenerator } from 'src/application/cache/key.generator/provided_port/cache.key.generator';
+import { type ICacheRegistry } from 'src/application/cache/registry/provided_port/cache.registry.interface';
+import { AbstractCacheInstance } from 'src/application/cache/cache.instance/provided_port/cache.instance.interface';
 
 @Injectable()
 export class OneInchInfoProvider extends AbstractDefiProtocolInfoProvider implements OnModuleInit{
-    private tokenCache = new Map<string, Token>()
+    private tokenCache : AbstractCacheInstance<string, Token> | null
 
     private supportingChains: ChainInfo[]
     private supportingTokens: Token[] = []
@@ -20,8 +24,13 @@ export class OneInchInfoProvider extends AbstractDefiProtocolInfoProvider implem
         private readonly httpClient: IHttpClient,
         @Inject(ONE_INCH_INFO_FETCHER)
         private readonly oneInchInfoFetcher: IOneInchInfoFetcher,
+        @Inject(ONE_INCH_INFO_PROVIDER_TOKEN_CACHE_KEY_GENERATOR)
+        private readonly tokenCacheKeyGenerator: ICacheKeyGenerator<string, KeyInput>,
+        @Inject(CACHE_REGISTRY)
+        cacheRegistry: ICacheRegistry,
     ) {
         super()
+        this.tokenCache = cacheRegistry.getCacheInstance(ONE_INCH_INFO_PROVIDER_TOKEN_CACHE_NAME)
     }
 
     async onModuleInit(): Promise<void> {
@@ -35,8 +44,10 @@ export class OneInchInfoProvider extends AbstractDefiProtocolInfoProvider implem
     
     private async setChainIdTokenMap() {
         this.supportingTokens.map((token) => {
-            const key = this.makeTokenCacheKey(token.chain.id, token.address)
-            this.tokenCache.set(key, token)
+            const key = this.tokenCacheKeyGenerator.genKey(
+                { chainId: token.chain.id, tokenAddress:  token.address}
+            )
+            this.tokenCache?.save(key, token)
         })
     }
 
@@ -81,9 +92,21 @@ export class OneInchInfoProvider extends AbstractDefiProtocolInfoProvider implem
         return result ?? null
     }
 
-    async getSupportingToken(chinId: number, tokenAddress: EvmAddress): Promise<Token | null> {
-        const key = this.makeTokenCacheKey(chinId, tokenAddress)
-        return this.tokenCache.get(key) ?? null
+    async getSupportingToken(chainId: number, tokenAddress: EvmAddress): Promise<Token | null> {
+        return this.getSupportingTokenFromCache(chainId, tokenAddress) ?? 
+            this.getSupportingTokenFromArray(chainId, tokenAddress)
     }
 
+    private async getSupportingTokenFromCache(chainId: number, tokenAddress: EvmAddress):  Promise<Token | null> {
+        const key = this.tokenCacheKeyGenerator.genKey(
+            { chainId: chainId, tokenAddress: tokenAddress}
+        )
+        return this.tokenCache?.get(key) ?? null
+    }
+
+    private async getSupportingTokenFromArray(chainId: number, tokenAddress: EvmAddress):  Promise<Token | null> {
+        return this.supportingTokens.find((token) => {
+            return (token.chain.id === chainId) && (token.address.equals(tokenAddress))
+        }) ?? null
+    }
 }
