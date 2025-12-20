fe94303dd22dca50de9e7aab5614fd3069a2a814
eyoon <euiseonyoon@gmail.com>
2025-12-20 15:04:55 +0900

    feat: make `EvmAddress.validateAddress()` static

--- src/domain/evm-address.class.ts
+++ src/domain/evm-address.class.ts
@@ -4,20 +4,19 @@ export class EvmAddress {
     constructor(address: string) {
         const cleanAddress = address.trim();
         
-        this.validateAddress(cleanAddress)
-        
-        this.address = cleanAddress.toLowerCase();
-    }
-
-    private validateAddress(address: string) {
-        const EVM_ADDRESS_REGEX = /^0x[a-fA-F0-9]{40}$/;
-
-        if (!EVM_ADDRESS_REGEX.test(address)) {
+        if (!EvmAddress.validateAddress(cleanAddress)) {
             throw new Error(
                 `Invalid EVM address format: ${address}. 
                 Must start with '0x' and be 42 characters long (40 hex digits).`
             );
         }
+        
+        this.address = cleanAddress.toLowerCase();
+    }
+
+    static validateAddress(address: string): boolean {
+        const EVM_ADDRESS_REGEX = /^0x[a-fA-F0-9]{40}$/;
+        return EVM_ADDRESS_REGEX.test(address)
     }
 
     public equals(other: string | EvmAddress): boolean {
