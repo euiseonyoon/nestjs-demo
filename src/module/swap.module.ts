60656bfca640dc8a4d682438f07cac8f2b854d5b
eyoon <euiseonyoon@gmail.com>
2025-12-21 20:33:45 +0900

    feat: add SushiSwapService to SWAP_SERVICES

--- src/module/swap.module.ts
+++ src/module/swap.module.ts
@@ -1,22 +1,28 @@
 import { Module } from '@nestjs/common';
 import { OneInchService } from '../application/swaps/1inch/1inch-swap.service';
-import { HttpClientModule } from './http-client.module';
 import { InfoProviderModule } from './info-provider/info-provider.module';
 import { AbstractSwapService } from 'src/application/swaps/provided_port/swap.interface';
 import { SwapQuoterModule } from './swap.quoter.module';
 import { SwapAmountGetterModule } from './swap.amount-getter.module';
 import { SWAP_SERVICES } from './module.token';
+import { SushiSwapService } from 'src/application/swaps/sushi-swap/sushi-swap.service';
 
 @Module({
-    imports: [HttpClientModule, InfoProviderModule, SwapQuoterModule, SwapAmountGetterModule],
+    imports: [
+        InfoProviderModule,
+        SwapQuoterModule, 
+        SwapAmountGetterModule,
+    ],
     providers: [
         OneInchService,
+        SushiSwapService,
         {
             provide: SWAP_SERVICES,
             inject: [
-                OneInchService, 
+                OneInchService,
+                SushiSwapService,
             ],
-            useFactory: (oneInchSwap): AbstractSwapService[] => [oneInchSwap],
+            useFactory: (oneInchSwap, sushiSwap): AbstractSwapService[] => [oneInchSwap, sushiSwap],
         }
     ],
     exports: [
