d208df7eae815ece5fb48b682500391312486c2c
eyoon <euiseonyoon@gmail.com>
2025-12-26 21:09:39 +0900

    feat: add StargateInfoFetcher to a info fetcher module

--- src/module/info-fetcher.module.ts
+++ src/module/info-fetcher.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { HttpClientModule } from './http-client.module';
 import { OneInchInfoFetcher } from 'src/application/info-fetcher/swap/1inch/1inch-swap.info-fetcher';
-import { ERC20_INFO_FETCHER, ONE_INCH_INFO_FETCHER, STABLE_COIN_INFO_FETCHER, SUSHI_SWAP_INFO_FETCHER } from './module.token';
+import { ERC20_INFO_FETCHER, ONE_INCH_INFO_FETCHER, STABLE_COIN_INFO_FETCHER, STARGATE_INFO_FETCHER, SUSHI_SWAP_INFO_FETCHER } from './module.token';
 import { SushiSwapInfoFetcher } from 'src/application/info-fetcher/swap/sushi-swap/sushi-swap.info-fetcher';
 import { PublicClientModule } from './public-client.module';
 import { Erc20InfoFetcher } from 'src/application/info-fetcher/erc20/erc20.info-fetcher';
 import { StableCoinInfoFetcher } from 'src/application/info-fetcher/stable-coin/info-fetcher';
+import { StargateInfoFetcher } from 'src/application/info-fetcher/bridge/stargate/stargate.info-fetcher';
 
 @Module({
     imports: [
@@ -29,12 +30,17 @@ import { StableCoinInfoFetcher } from 'src/application/info-fetcher/stable-coin/
             provide: STABLE_COIN_INFO_FETCHER,
             useClass: StableCoinInfoFetcher,
         },
+        {
+            provide: STARGATE_INFO_FETCHER,
+            useClass: StargateInfoFetcher,
+        },
     ],
     exports: [
         ONE_INCH_INFO_FETCHER,
         ERC20_INFO_FETCHER,
         SUSHI_SWAP_INFO_FETCHER,
         STABLE_COIN_INFO_FETCHER,
+        STARGATE_INFO_FETCHER,
     ],
 })
 export class InfoFetcherModule {}
